bc172571d232ac234a58c7b90c28e644a697a0a3
salami96 <gabriel.zanatto2@gmail.com>
2020-09-22 21:47:30 -0300

    starting address component

--- src/app/profile/address/address.component.ts
+++ src/app/profile/address/address.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { UserService } from '../../services/user.service';
 
 @Component({
   selector: 'app-address',
@@ -6,10 +7,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./address.component.css']
 })
 export class AddressComponent implements OnInit {
+  address: Address;
+  error = {
+    name: false,
+    street: false,
+    number: false,
+    district: false,
+    city: false,
+    state: false,
+    complement: false,
+    reference: false,
+    zipCode: false
+  };
 
-  constructor() { }
+  constructor(
+    private uService: UserService,
+  ) { }
 
   ngOnInit() {
+  }
+
+  focus(field: string) {
+    document.getElementById(field).focus();
   }
 
 }
