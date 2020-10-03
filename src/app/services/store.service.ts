bda79f5a44921adcdff2c8c333e41dc1bfef65bb
salami96 <gabriel.zanatto2@gmail.com>
2020-10-02 23:18:08 -0300

    fix error at redirect

--- src/app/services/store.service.ts
+++ src/app/services/store.service.ts
@@ -223,7 +223,8 @@ export class StoreService {
   loadStore(code: string) {
     this.http.get<Store>(this.url + 'store/' + code, this.options).subscribe(resp => {
       this._storeObserver.next(resp);
-    }, error => window.location.href = 'https://copac.produtos.app');
+    }, error => console.log(error));
+    // }, error => window.location.href = 'https://copac.produtos.app');
   }
 
 }
