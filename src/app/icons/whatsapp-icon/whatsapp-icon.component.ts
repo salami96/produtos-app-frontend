d38d30ba6e326ea5942cfff4ae2318a007eb957b
Gabriel Zanatto Salami <gabriel.zanatto2@gmail.com>
2021-05-25 09:57:54 -0300

    fix: improve whatsapp icon

--- src/app/icons/whatsapp-icon/whatsapp-icon.component.ts
+++ src/app/icons/whatsapp-icon/whatsapp-icon.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   // tslint:disable-next-line: component-selector
@@ -7,10 +7,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./whatsapp-icon.component.css']
 })
 export class IconWhatsappComponent implements OnInit {
+  @Input() size: string
 
   constructor() { }
 
   ngOnInit() {
+    if (!this.size) {
+      this.size = '24px';
+    }
   }
 
 }
